449677edfc91ae1590263ef04b0b8bb87a4b8b57
Sunderamurthy <sunderamurthy1996@gmail.com>
2023-09-03 19:11:10 -0400

    Cart Functionality Completed

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -4,10 +4,12 @@ import { useContext } from 'react';
 import CartContext from '../store/cart-context';
 
 const HeaderCartButton = (props)=>{
-    const cartCtx = useContext(CartContext)
+    const cartCtx = useContext(CartContext);
+    
     const numberOfCartItems = cartCtx.items.reduce((currNumber,item)=>{
         return currNumber + item.amount;
     },0);
+    
     return<button className={classes.button} onClick={props.onClick}>
         <span className={classes.icon}>
             <CartIcon/>
